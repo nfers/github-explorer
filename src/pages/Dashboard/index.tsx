4eaa399fde51b02196eff410659abd9f5bd8f7af
Nayara Ferreira <ncy.fers@gmail.com>
2020-07-12 22:12:25 -0300

    adicionando formulario de pesquisa dos repositorios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import { Title } from './style';
+import { Title, Form, Input } from './style';
+
+import logo from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  return (
+    <>
+      <img src={logo} alt="Logo Projeto" />
+      <Title>Dashboard</Title>
+      <Form>
+        <Input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+    </>
+  );
 };
 
 export default Dashboard;
